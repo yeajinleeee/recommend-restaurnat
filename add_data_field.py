2856e25fe987b5bd7323eca4ea1059bea7132153
yeajinleeee <162455065+yeajinleeee@users.noreply.github.com>
2025-07-24 20:20:15 +0900

    Update add_data_field.py

--- add_data_field.py
+++ add_data_field.py
@@ -21,7 +21,6 @@ def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument('--no-sandbox')
     options.add_argument('disable-gpu')
-    options.add_argument('headless')
     options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(options=options)
     return driver
