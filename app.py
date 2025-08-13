97c31fd543abcf923af109c13273e7c1be0b34e1
yeajinleeee <162455065+yeajinleeee@users.noreply.github.com>
2025-08-13 18:51:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ supabase: Client = create_client(url, key)
 st.title("날씨 + 위치 기반 음식점 추천🌨️")
 
 #위치 가져오기 
-location = streamlit_geolocation(high_accuracy=True)
+location = streamlit_geolocation()
 
 if location:
     user_lat = location["latitude"]
