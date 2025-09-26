e8a6f05e9a47fc4e2858d48183b9dc3635a4b798
Hyemin Jung <jungheamin01@naver.com>
2025-09-26 20:38:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -294,7 +294,7 @@ def main():
                                       get_fill_color=[255,0,0,160], pickable=True)]
                 ))
 
-        col1, col2 = st.columns([[9.5, 0.5])
+        col1, col2 = st.columns([[9.5, 0.5]])
         with col1:
             if st.button("⬅ 이전"):
                 st.session_state.page = "page1"
